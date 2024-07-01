e396b1b41c3dd046031e99b10402a89f73a55970
TECH VALLEY <TECH VALLEY@DESKTOP-CUMGKU2>
2024-07-02 00:52:33 +0300

    Question6

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,25 +1,56 @@
-﻿namespace Assignment
+﻿using System.ComponentModel;
+using System.Drawing;
+using System.Reflection;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Assignment
 {
     internal class Program 
    
     {
         #region Question1
+
+        // Passing by Value  When a value type parameter is passed by value,
+        // a copy of the actual value is passed to the function.
+        // This means that any changes made to the parameter inside the function
+        // do not affect the original value outside the function.
+
+        //Passing by Reference
+       // When a value type parameter is passed by reference,
+       // a reference to the actual memory location of the value is passed to the function.
+       // This means that any changes made to the parameter inside the function
+       // do affect the original value outside the function
+
+
         //static void SWAP(int x, int y)
         //{
         //    int tem = x;
         //    x = y;
         //    y = tem;
         //}
-        static void SWAP(ref int x,ref int y)
-        {
-            int tem = x;
-            x = y;
-            y = tem;
-        }
+        //static void SWAP(ref int x,ref int y)
+        //{
+        //    int tem = x;
+        //    x = y;
+        //    y = tem;
+        //}
         #endregion
 
 
         #region Question2
+        // Passing Reference Type Parameters by Value
+        //  When a reference type parameter is passed by value, a copy of the reference(not the object itself)
+        //  is passed to the method.This means that while the method cannot change the reference itself
+        //  to point to a new object, it can modify the object that the reference points to.
+
+
+        //Passing Reference Type Parameters by Value
+        //When a reference type parameter is passed by value, a copy of the reference(not the object itself)
+        //is passed to the method.This means that while the method cannot change the reference itself to point
+        //to a new object, it can modify the object that the reference points to.
+
+
+
         //static int SumArray(int[] arr)
         //{
         //    int sum = 0;
@@ -54,18 +85,85 @@
         #endregion
 
         #region Question4
-       //static int SumDigit(int num)
-       // {
-       //     int sum = 0;
-       //     while (num is not 0)
-       //     {
-       //        sum+= num % 10;
-       //         num /= 10;
-                   
-       //     }
-       //     return sum; 
-       // }
+        //static int SumDigit(int num)
+        // {
+        //     int sum = 0;
+        //     while (num is not 0)
+        //     {
+        //        sum+= num % 10;
+        //         num /= 10;
+
+        //     }
+        //     return sum; 
+        // }
         #endregion
+
+        #region Question5
+        //use the Sieve of Eratosthenes
+        //static bool IsPrime(int number)
+        //{
+        //    if (number <= 1) return false;
+        //    if (number <= 3) return true;
+
+        //    if (number % 2 == 0 || number % 3 == 0) return false;
+
+        //    int sqrt = (int)Math.Sqrt(number);
+        //    bool[] sieve = new bool[sqrt + 1];
+        //    for (int i = 2; i <= sqrt; i++)
+        //    {
+        //        sieve[i] = true;
+        //    }
+        //    for (int i = 2; i * i <= sqrt; i++)
+        //    {
+        //        if (sieve[i])
+        //        {
+        //            for (int j = i * i; j <= sqrt; j += i)
+        //            {
+        //                sieve[j] = false;
+        //            }
+        //        }
+        //    }
+
+        //    for (int i = 2; i <= sqrt; i++)
+        //    {
+        //        if (sieve[i] && number % i == 0)
+        //        {
+        //            return false;
+        //        }
+        //    }
+
+        //    return true;
+        //}
+        #endregion
+
+        #region Question6
+        //6 Create a function named MinMaxArray, to return the minimum and maximum values
+        //stored in an array, using reference parameters
+        //static void MinMaxArray(  out int max, out int min , params int[] arr)
+        //{
+        //    max = arr[0];
+        //    min = arr[0];
+
+        //    for (int i = 0; i < arr.Length; i++)
+        //    {
+               
+        //        if (arr[i] > max)
+        //        {
+        //            max = arr[i];
+        //        }
+
+        //        if (arr[i] < min)
+        //        {
+        //            min = arr[i];
+        //        }
+        //    }
+        //}
+
+
+        #endregion
+
+
+
         static void Main(string[] args)
         {
             #region Question1
@@ -122,6 +220,22 @@
             //n=int.Parse( Console.ReadLine());
             //Console.WriteLine(SumDigit(n));
             #endregion
+
+            #region Question5
+            //5 Create a function named "IsPrime", which receives an integer number and
+            //retuns true if it is prime, or false if it is not:
+            //int num;
+            //num=int.Parse(Console.ReadLine());
+            //bool res= IsPrime(num);
+            //Console.WriteLine(res);
+            #endregion
+   
+            //int min, max;
+            //MinMaxArray(out max,out min, 1,2,3,4,5);
+            //Console.WriteLine($"Minimum value: {min}");
+            //Console.WriteLine($"Maximum value: {max}");
+
+
         }
     }
 }
