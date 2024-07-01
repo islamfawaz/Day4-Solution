c7771c22312c38143b571678686e29fb94c0d3e2
TECH VALLEY <TECH VALLEY@DESKTOP-CUMGKU2>
2024-07-01 17:03:38 +0300

    Question1

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,10 +1,43 @@
 ﻿namespace Assignment
 {
-    internal class Program
+    internal class Program 
+   
     {
+        #region Question1
+        //static void SWAP(int x, int y)
+        //{
+        //    int tem = x;
+        //    x = y;
+        //    y = tem;
+        //}
+        static void SWAP(ref int x,ref int y)
+        {
+            int tem = x;
+            x = y;
+            y = tem;
+        }
+        #endregion
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question1
+            //1 Explain the difference between passing (Value type parameters) by
+            //value and by reference then write a suitable c# example.
+            //int A = 3,B=5;
+            //SWAP(A,B);
+            //Console.WriteLine("After swap passing by value");
+            //Console.WriteLine($"A = {A}");
+            //Console.WriteLine($"B= {B}");
+
+            //int A = 3, B = 5;
+            //SWAP(ref A, ref B);
+            //Console.WriteLine("After swap passing by ref");
+            //Console.WriteLine($"A = {A}");
+            //Console.WriteLine($"B= {B}");
+
+
+
+
+            #endregion  
         }
     }
 }
